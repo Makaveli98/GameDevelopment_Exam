d53f163ad2460280f6b8f23bb3c330ee14e5f26e
Renato <renato.daman1988@gmail.com>
2021-04-19 23:46:39 +0200

    worked further with the animations, made a few animations work and put the animation function in this script

--- FP_Shooter/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ FP_Shooter/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     private CharacterController character_Controller;
-
     private PlayerFootsteps player_Footsteps;
+    private WeaponManager weapon_Manager;
+    private PlayerSprintAndCrouch sprint_Crouch;
+
 
     [HideInInspector]
     public Vector3 move_Direction;
 
-    public float speed = 5f;
+    private Vector3 test_Direction;
+
+    public float speed = 10f;
     private float gravity = 20f;
 
     public float jump_Force = 10f;
@@ -22,6 +26,9 @@ public class PlayerMovement : MonoBehaviour {
 
         character_Controller = GetComponent<CharacterController>();
         player_Footsteps = GetComponentInChildren<PlayerFootsteps>();
+        weapon_Manager = GetComponent<WeaponManager>();
+        sprint_Crouch = GetComponent<PlayerSprintAndCrouch>();
+        
     }
 
     void Update() {
@@ -30,15 +37,32 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void MoveThePlayer() {
-
         move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL)); // this will move the gameobject (player) down the x and z axis
 
-        move_Direction = transform.TransformDirection(move_Direction); // this will tell the gameobject to move to a certain position, in this case at any direction
+        move_Direction = transform.TransformDirection(move_Direction); // this will tell the gameobject to move from local space to world space
         move_Direction *= speed * Time.deltaTime; // tells the gameobject at what speed it will move
 
         ApllyGravity();  // applies gravity
         
         character_Controller.Move(move_Direction); // and this will make the gameobject actually move
+    
+        // if move then play WALK anim
+        if (character_Controller.velocity.sqrMagnitude > 0) {
+            weapon_Manager.GetCurrentSelectedWeapon().Play_WalkAnimation();
+        // if not move then play IDLE anim
+        } else if (character_Controller.velocity.sqrMagnitude <= 0) {
+            weapon_Manager.GetCurrentSelectedWeapon().Stop_WalkAnimation();            
+        }
+
+        // if move and crouching then play CROUCH anim
+        if (character_Controller.velocity.sqrMagnitude > 0 && sprint_Crouch.is_Crouching) {
+            weapon_Manager.GetCurrentSelectedWeapon().Play_CrouchAnimation();
+        } 
+
+        // if move and sprinting then play SPRINT anim
+        if (character_Controller.velocity.sqrMagnitude > 0 && sprint_Crouch.is_Sprinting) {
+            weapon_Manager.GetCurrentSelectedWeapon().Play_RunAnimation();
+        } 
 
     } // move player
 
@@ -57,6 +81,10 @@ public class PlayerMovement : MonoBehaviour {
         
         if (character_Controller.isGrounded && Input.GetKeyDown(KeyCode.Space)) {
             vertical_Velocity = jump_Force;
+
+            if (character_Controller.velocity.sqrMagnitude <= 0) {
+                weapon_Manager.GetCurrentSelectedWeapon().Stop_WalkAnimation();            
+            }
 
             if (character_Controller.isGrounded) { 
                 player_Footsteps.Player_Fall_Sound();
